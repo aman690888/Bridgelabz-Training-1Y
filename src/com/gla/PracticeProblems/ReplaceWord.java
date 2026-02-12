408f24d011d8766e7114d3b3c635a499a8516538
aman690888 <ar3655185@gmail.com>
2026-02-12 22:42:42 +0530

    added practice problems

---
+++ src/com/gla/PracticeProblems/ReplaceWord.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ReplaceWord {
+
+    public static String replace(String sentence, String oldWord, String newWord) {
+
+        String[] words = sentence.split(" ");
+        String result = "";
+
+        for (String w : words) {
+
+            if (w.equals(oldWord))
+                result += newWord + " ";
+            else
+                result += w + " ";
+        }
+
+        return result.trim();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String s = sc.nextLine();
+
+        System.out.print("Word to replace: ");
+        String oldWord = sc.next();
+
+        System.out.print("New word: ");
+        String newWord = sc.next();
+
+        String res = replace(s, oldWord, newWord);
+
+        System.out.println("Result: " + res);
+
+        sc.close();
+    }
+}
